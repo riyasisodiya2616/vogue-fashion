312c22c92f2a45d01883216ff842ecb5e8964c80
riyasisodiya2616 <149227776+riyasisodiya2616@users.noreply.github.com>
2024-03-01 15:59:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// modal-section
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    const button = event.relatedTarget
+
+    const recipient = button.getAttribute('data-bs-whatever')
+ 
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
